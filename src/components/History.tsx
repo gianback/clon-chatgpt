bfb29d40442302caef439660fc4bc51baffdf261
Gian Franco <gianfraback@gmail.com>
2023-08-17 03:03:13 -0500

    fix:  overflow y scroll was fixed

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -5,17 +5,21 @@ import { OpenaiIcon } from "./Icons";
 export const History = () => {
   const history = usePromptStore((state) => state.history);
   return (
-    <ul className="max-w-[48rem] px-4 lg:px-0 w-full mx-auto h-[44rem]">
-      {history?.map(({ id, prompt }) => (
+    <ul className="w-full h-full mx-auto flex flex-col items-center overflow-y-auto">
+      {history?.map(({ id, prompt }, index) => (
         <li
           key={id}
-          className="text-white flex gap-8 py-6 w-[48rem] max-w-full"
+          className={`text-white px-4 lg:px-0 w-full flex justify-center ${
+            (index + 1) % 2 !== 0 ? "bg-primary" : "bg-quinary"
+          }`}
         >
-          <div className="bg-[#19c37d] w-[30px] h-[30px] flex-shrink-0 flex items-center justify-center">
-            <OpenaiIcon />
-          </div>
-          <div>
-            <ReactMarkdown>{prompt}</ReactMarkdown>
+          <div className="max-w-[48rem] flex gap-8 py-6 w-[48rem] ">
+            <div className="bg-[#19c37d] w-[30px] h-[30px] flex-shrink-0 flex items-center justify-center">
+              <OpenaiIcon />
+            </div>
+            <div>
+              <ReactMarkdown>{prompt}</ReactMarkdown>
+            </div>
           </div>
         </li>
       ))}
