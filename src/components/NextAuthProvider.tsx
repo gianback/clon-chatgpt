06a8d1a9bf26a080da9ac23eb8f9c48a4301f06c
Gian Franco <gianfraback@gmail.com>
2023-08-19 03:16:46 -0500

    fix: use client to next auth provider

--- src/components/NextAuthProvider.tsx
+++ src/components/NextAuthProvider.tsx
@@ -1,8 +1,9 @@
+"use client";
 import { SessionProvider } from "next-auth/react";
 
 type Props = {
-    children?: React.ReactNode;
-  };
-  export function NextAuthProvider ({ children }:Props)  {
-    return <SessionProvider>{children}</SessionProvider>;
-  };
+  children?: React.ReactNode;
+};
+export function NextAuthProvider({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
