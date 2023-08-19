84a9c34a4a593f4d02dc49a0dee111d0be266afd
Gian Franco <gianfraback@gmail.com>
2023-08-19 03:16:21 -0500

    refactor: update auth options

--- src/utilities/auth.ts
+++ src/utilities/auth.ts
@@ -1,29 +1,12 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
   providers: [
-    CredentialsProvider({
-      name: "Sign in",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-        },
-      },
-      async authorize(credentials) {
-        const user = { id: "1", name: "Admin", email: "admin@admin.com" };
-        return user;
-      },
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_PROVIDER_ID || "",
       clientSecret: process.env.GITHUB_PROVIDER_SECRET || "",
