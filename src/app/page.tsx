04d5a516dc74b5ade534721cca87075d61d1b52c
Gian Franco <gianfraback@gmail.com>
2023-08-21 03:08:26 -0500

    refactor: add presentation component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,21 @@
-"use client";
-import { Form, History } from "@/components";
-import { usePromptStore } from "@/store/PromptStore";
+import { authOptions } from "@/utilities/auth";
+import { NextPageContext } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { Header } from "@/components/Header";
+import { Presentation } from "@/components/Presentation";
 
-export default function Home() {
-  const history = usePromptStore((state) => state.history);
+export default async function Home(cxt: NextPageContext) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/auth/signin");
+  }
 
+  console.log(session);
   return (
-    <div className="relative flex min-h-[94vh] max-w-full flex-1 overflow-hidden bg-primary">
-      <div className="flex h-full min-h-[94vh] max-w-full flex-1 flex-col">
-        <main className="relative min-h-[inherit] w-full transition-width flex flex-col overflow-auto items-stretch flex-1">
-          <h2
-            className={`${
-              history.length > 0 && "hidden"
-            } text-white text-4xl font-semibold text-center mt-6 sm:mt-[6vh] ml-auto mr-auto mb-4 sm:mb-16 flex gap-2 items-center justify-center`}
-          >
-            ChatGPT
-          </h2>
-          <div className="h-full flex flex-col ">
-            <History />
-            <div className="h-[60px] lg:h-[90px] w-full" />
-            <div className="bg-primary absolute bottom-0 left-0 w-full border-t border-tertiary md:border-transparent md:dark:border-transparent md:bg-vert-light-gradien dark:bg-gray-800 dark:md:bg-vert-dark-gradient pt-2 pb-2 md:pl-2 md:w-[calc(100%-1.5rem)]">
-              <Form />
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
+    <main className="h-screen overflow-hidden w-full relative flex flex-col z-0 lg:flex-row">
+      <Header />
+      <Presentation />
+    </main>
   );
 }
