477dfc2a28633f862b7c8cd8994372d5bb7fe29b
Gian Franco <gianfraback@gmail.com>
2023-08-21 03:10:18 -0500

    feat: add user from session and avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 import { CloseMenuDesk, Dots, HamburguersMenu, PlusIcon, XIcon } from "./Icons";
 
 export function Header() {
   const [menuActive, setMenuActive] = useState(false);
+  const { data: session } = useSession();
 
   return (
     <>
@@ -31,7 +33,7 @@ export function Header() {
          
           `}
         >
-          <div className="flex h-full min-h-0 flex-col ">
+          <div className="flex h-full min-h-0 flex-col w-full">
             <div className="scrollbar-trigger relative h-full w-full flex-1 items-start border-white/20">
               <nav
                 className="flex h-full w-full flex-col p-2"
@@ -65,10 +67,24 @@ export function Header() {
                   <div className="group relative" data-headlessui-state="">
                     <button className="flex w-full items-center gap-2.5 rounded-md px-3 py-3 text-sm transition-colors duration-200 hover:bg-gray-800 group-ui-open:bg-gray-800">
                       <div className="flex-shrink-0">
-                        <div className="relative flex">imagen user</div>
+                        {session ? (
+                          <picture className="relative flex justify-center align-center">
+                            <img
+                              src={session?.user?.image as string}
+                              alt=""
+                              className="w-8"
+                            />
+                          </picture>
+                        ) : (
+                          <div className="w-8 h-8 bg-primary animate-pulse" />
+                        )}
                       </div>
                       <div className="grow overflow-hidden text-ellipsis whitespace-nowrap text-left text-white">
-                        gian_franco_08@hotmail.com
+                        {session ? (
+                          <span>{session?.user?.email}</span>
+                        ) : (
+                          <div className="w-40 h-8 bg-primary animate-pulse"></div>
+                        )}
                       </div>
                       <Dots />
                     </button>
