f0288d29499fc7f35f3ad4295663ad537cdf68c3
Gian Franco <gianfraback@gmail.com>
2023-08-21 03:09:54 -0500

    refactor: change code to Button component

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,26 +1,22 @@
-"use client";
-import { GithubIcon, GoogleIcon } from "@/components";
-import { signIn } from "next-auth/react";
-export default function SignIn() {
+import { Button } from "@/components/Buttons";
+import { authOptions } from "@/utilities/auth";
+import { getServerSession } from "next-auth";
+
+import { redirect } from "next/navigation";
+export default async function SignIn() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/");
+  }
   return (
     <main className="h-screen bg-primary w-full flex items-center justify-center flex-col">
-      <h1 className="text-5xl font-medium mb-16 text-white">
+      <h1 className="text-5xl font-medium mb-16 text-white text-center">
         Log in to Chatgpt
       </h1>
-      <button
-        className="flex gap-3 bg-black hover:bg-black/80 rounded-lg items-center justify-center text-white py-3 px-9"
-        onClick={() => signIn("github")}
-      >
-        <GithubIcon />
-        <span>Continue with GitHub</span>
-      </button>
-      <button
-        className="flex mt-4 gap-3 bg-black hover:bg-black/80 rounded-lg items-center justify-center text-white py-3 px-9"
-        onClick={() => signIn("github")}
-      >
-        <GoogleIcon />
-        <span>Continue with Google</span>
-      </button>
+      <div className="flex flex-col gap-4">
+        <Button type="github" />
+        <Button type="google" />
+      </div>
     </main>
   );
 }
