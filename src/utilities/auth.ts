d872d3743b8e4dcb1b09a33442d761bee7dd5a8b
Gian Franco <gianfraback@gmail.com>
2023-08-21 03:11:10 -0500

    feat: add callback to session

--- src/utilities/auth.ts
+++ src/utilities/auth.ts
@@ -1,11 +1,13 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_PROVIDER_ID || "",
@@ -16,6 +18,17 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
   pages: {
     signIn: "/auth/signin",
   },
