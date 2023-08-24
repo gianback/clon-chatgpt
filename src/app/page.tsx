773ebc03a30d273859c6bdedde976229ca0015a8
Gian Franco <gianfraback@gmail.com>
2023-08-24 03:15:48 -0500

    feat: add overlay component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 import { authOptions } from "@/utilities/auth";
-import { NextPageContext } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { Header } from "@/components/Header";
 import { Presentation } from "@/components/Presentation";
+import { Overlay } from "@/components/Overlay";
+import { History } from "@/components/History";
 
-export default async function Home(cxt: NextPageContext) {
+export default async function Home() {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/auth/signin");
   }
 
-  console.log(session);
   return (
     <main className="h-screen overflow-hidden w-full relative flex flex-col z-0 lg:flex-row">
-      <Header />
+      <Header>
+        <History />
+      </Header>
       <Presentation />
+      <Overlay />
     </main>
   );
 }
