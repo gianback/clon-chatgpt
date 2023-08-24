888c963666c0f67492c202c11e61d54e68fdb98b
Gian Franco <gianfraback@gmail.com>
2023-08-24 03:17:43 -0500

    refactor: refactor code form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,40 @@
 "use client";
 import { FormEvent } from "react";
-import { SendPrompt } from "./Icons";
-import { getAnswerService } from "@/services/";
-import { usePromptStore } from "@/store/PromptStore";
-import crypto from "crypto";
+import { SendPromptIcon } from "./Icons";
+import { createAnswerService } from "@/services/";
+import { createQueryService } from "@/services/create.query.service";
+import { useSession } from "next-auth/react";
+import { useCurrentQuery } from "@/store/CurrentQuery";
+import { updateQueryService } from "@/services/update.query.service";
 
 export function Form() {
-  const { setPrompt } = usePromptStore();
+  const { data: userData } = useSession();
+  const { setCurrentId, currentId, setQueryList } = useCurrentQuery();
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const form = e.currentTarget;
     const formData = new FormData(form);
-    const input = formData.get("input") as string;
-    setPrompt({ id: window.crypto.randomUUID(), prompt: input });
+    const prompt = formData.get("input") as string;
     form.reset();
-    const response = await getAnswerService(input);
-    setPrompt({ id: window.crypto.randomUUID(), prompt: response });
+
+    //answer
+    const answer = await createAnswerService(prompt);
+
+    if (!currentId) {
+      const historyItem = await createQueryService({
+        historyItem: [prompt, answer],
+        userId: userData?.user.id,
+      });
+      setCurrentId(historyItem.id);
+      setQueryList(historyItem.querys);
+    } else {
+      const updatedHistoryItem = await updateQueryService({
+        historyItemid: currentId,
+        newQuery: [prompt, answer],
+      });
+      setQueryList(updatedHistoryItem.querys);
+    }
   };
 
   return (
@@ -35,7 +52,7 @@ export function Form() {
           type="submit"
           className="top-[50%] cursor-pointer translate-y-[-50%] absolute flex items-center justify-center p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 enabled:bg-brand-purple text-white bottom-1.5 transition-colors disabled:opacity-40"
         >
-          <SendPrompt />
+          <SendPromptIcon />
         </button>
       </div>
     </form>
