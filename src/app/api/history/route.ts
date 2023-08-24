1a8b8f226b23732991a7af3f5ec2803ed997895d
Gian Franco <gianfraback@gmail.com>
2023-08-24 03:16:26 -0500

    refactor: change route name

---
+++ src/app/api/history/route.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@/utilities/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { historyItem, userId } = await request.json();
+
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+
+  if (!user)
+    return new Response("User not found", {
+      status: 404,
+    });
+
+  const queryListCreated = await prisma.historyItem.create({
+    data: {
+      querys: historyItem,
+      userId,
+    },
+  });
+
+  return NextResponse.json({
+    mssg: "Query created",
+    query: queryListCreated,
+  });
+}
+
+export async function PATCH(request: Request) {
+  const { historyItemid, newQuery } = await request.json();
+  const query = await prisma.historyItem.updateMany({
+    where: {
+      id: historyItemid,
+    },
+    data: {
+      querys: {
+        push: newQuery,
+      },
+    },
+  });
+
+  return NextResponse.json({
+    mss: "Query added",
+    query,
+  });
+}
