9edf332309af72191ae509fcda9c4240841c140e
Gian Franco <gianfraback@gmail.com>
2023-08-24 03:16:58 -0500

    feat: create currentHistory component

---
+++ src/components/CurrentHistory.tsx
@@ -0,0 +1,38 @@
+"use client";
+import ReactMarkdown from "react-markdown";
+import { OpenaiIcon } from ".";
+import { useSession } from "next-auth/react";
+import { useCurrentQuery } from "@/store/CurrentQuery";
+
+export const CurrentHistory = () => {
+  const { queryList } = useCurrentQuery();
+  const session = useSession();
+
+  return (
+    <ul className="w-full h-full mx-auto flex flex-col items-center overflow-y-auto">
+      {queryList?.map((prompt, index) => (
+        <li
+          key={prompt + index}
+          className={`text-white px-4 lg:px-0 w-full flex justify-center ${
+            (index + 1) % 2 !== 0 ? "bg-primary" : "bg-quinary"
+          }`}
+        >
+          <div className="max-w-[48rem] flex gap-8 py-6 w-[48rem] ">
+            <div className="bg-[#19c37d] w-[30px] h-[30px] flex-shrink-0 flex items-center justify-center">
+              {(index + 1) % 2 !== 0 ? (
+                <OpenaiIcon />
+              ) : (
+                <picture>
+                  <img src={session.data?.user.image} alt="user image" />
+                </picture>
+              )}
+            </div>
+            <div>
+              <ReactMarkdown>{prompt}</ReactMarkdown>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
