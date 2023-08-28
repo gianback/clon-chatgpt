d7f5f438b0c61e9ce318493875660ae0a5f02d5c
Gian Franco <gianfraback@gmail.com>
2023-08-28 02:23:40 -0500

    refactor: swap rsc for client component

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,20 +1,32 @@
-import { getServerSession } from "next-auth";
+"use client";
 
-import { authOptions } from "@/utilities/auth.utility";
-import { User } from "@/interfaces/User";
+import { HistoryItem as HistoryItemType, User } from "@/interfaces/User";
 import { getHistoryByUseridService } from "@/services/getHistoryByUserId.service";
 import { HistoryItem } from "./HistoryItem";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useHistoryStore } from "@/store/HistoryStore";
 
-export async function History() {
-  const session = await getServerSession(authOptions);
-  const user = (await getHistoryByUseridService(session?.user.id)) as User;
+const getData = async (id: string) => {
+  const data = await getHistoryByUseridService(id);
+  const setHistory = useHistoryStore.getState().setHistory;
+  setHistory(data.history, false);
+};
+
+export function History() {
+  const { data } = useSession();
+  const history = useHistoryStore((state) => state.history);
+
+  useEffect(() => {
+    getData(data?.user.id);
+  }, [data?.user.id]);
 
   return (
     <div
       id="History"
       className="flex flex-col flex-1 transition-opacity duration-500 overflow-y-auto"
     >
-      {user.history?.map((historyItem) => (
+      {history?.map((historyItem) => (
         <HistoryItem key={historyItem.id} historyItem={historyItem} />
       ))}
     </div>
