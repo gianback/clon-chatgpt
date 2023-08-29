c9db16ef4be012edce142c9fc157df7753bc6926
Gian Franco <gianfraback@gmail.com>
2023-08-29 01:32:44 -0500

    refactor: create get history by user id utility

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,30 +1,27 @@
 "use client";
 
-import { HistoryItem as HistoryItemType, User } from "@/interfaces/User";
-import { getHistoryByUseridService } from "@/services/getHistoryByUserId.service";
-import { HistoryItem } from "./HistoryItem";
 import { useEffect } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useSession } from "next-auth/react";
-import { useHistoryStore } from "@/store/HistoryStore";
 
-const getData = async (id: string) => {
-  const data = await getHistoryByUseridService(id);
-  const setHistory = useHistoryStore.getState().setHistory;
-  setHistory(data.history, false);
-};
+import { HistoryItem } from "./HistoryItem";
+import { useHistoryStore } from "@/store";
+import { getHistoryByUserIdUtility } from "@/utilities/getHistoryByUserId.utility";
 
 export function History() {
   const { data } = useSession();
+  const [parent] = useAutoAnimate();
   const history = useHistoryStore((state) => state.history);
 
   useEffect(() => {
-    getData(data?.user.id);
+    getHistoryByUserIdUtility(data?.user.id);
   }, [data?.user.id]);
 
   return (
     <div
       id="History"
       className="flex flex-col flex-1 transition-opacity duration-500 overflow-y-auto"
+      ref={parent}
     >
       {history?.map((historyItem) => (
         <HistoryItem key={historyItem.id} historyItem={historyItem} />
