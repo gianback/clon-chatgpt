df5d465c8dbaf068a6b8fb27ad5d58462de25444
Gian Franco <gianfraback@gmail.com>
2023-09-03 04:01:46 -0500

    refactor: improve currentHistory

--- src/components/CurrentHistory.tsx
+++ src/components/CurrentHistory.tsx
@@ -5,32 +5,33 @@ import { useSession } from "next-auth/react";
 
 import { useCurrentQueryStore } from "@/store";
 import { OpenaiIcon } from ".";
+import { userStore } from "@/store/userStore";
 
 export const CurrentHistory = () => {
   const { queryList } = useCurrentQueryStore();
   const [parent] = useAutoAnimate();
-  const session = useSession();
+  const { picture } = userStore();
 
   return (
     <ul
       id="CurrentHistory"
-      className="w-full h-full mx-auto flex flex-col items-center overflow-y-auto"
+      className="w-full h-full mx-auto flex flex-col items-center overflow-y-auto overflow-x-hidden"
       ref={parent}
     >
       {queryList?.map((prompt, index) => (
         <li
           key={prompt + index}
-          className={`text-white px-4 lg:px-0 w-full flex justify-center ${
+          className={`text-white px-4 lg:px-0 w-full flex justify-center fadeIn ${
             (index + 1) % 2 !== 0 ? "bg-primary" : "bg-quinary"
           }`}
         >
           <div className="max-w-[48rem] flex gap-8 py-6 w-[48rem] ">
             <div className="bg-[#19c37d] w-[30px] h-[30px] flex-shrink-0 flex items-center justify-center">
-              {(index + 1) % 2 !== 0 ? (
+              {(index + 1) % 2 !== 1 ? (
                 <OpenaiIcon />
               ) : (
                 <picture>
-                  <img src={session.data?.user.image} alt="user image" />
+                  <img src={picture} alt="user image" />
                 </picture>
               )}
             </div>
