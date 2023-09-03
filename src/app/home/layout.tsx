e8759b319aa1553a09014ecb7d456ff6401335e1
Gian Franco <gianfraback@gmail.com>
2023-09-03 15:13:09 -0500

    refactor: remove UserStoreInitializer component

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { Header, HistorySection } from "@/components";
 
 import "../globals.css";
-import { Session, getServerSession } from "next-auth";
-import { authOptions } from "@/utilities/auth.utility";
-import { UserStoreInitializer } from "@/components/UserStoreInitializer";
 
 export const metadata: Metadata = {
   title: "Clone ChatGpt",
@@ -16,15 +13,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = (await getServerSession(authOptions)) as Session;
-
   return (
     <html lang="en">
       <body>
         <div className="h-screen overflow-hidden w-full relative flex flex-col z-0 lg:flex-row">
-          <UserStoreInitializer session={session} />
           <Header>
-            <HistorySection id={session?.user.id} />
+            <HistorySection />
           </Header>
           {children}
         </div>
