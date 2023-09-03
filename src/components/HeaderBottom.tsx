016a4822a65144bb0b689cbfbeefc3f705cbffef
Gian Franco <gianfraback@gmail.com>
2023-09-03 15:13:54 -0500

    refactor: user data from userStore

--- src/components/HeaderBottom.tsx
+++ src/components/HeaderBottom.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import { Dots, LogOutIcon } from ".";
 import { useState } from "react";
+import { userStore } from "@/store/userStore";
 
 export function HeaderBottom() {
-  const { data: session } = useSession();
+  const [email, picture] = userStore((state) => [state.email, state.picture]);
   const [isLogoutMenuActive, setIsLogoutMenuActive] = useState(false);
 
   return (
@@ -12,21 +13,17 @@ export function HeaderBottom() {
       <div className="group relative">
         <div className="flex w-full items-center gap-2.5 rounded-md px-3 py-3 text-sm transition-colors duration-200 hover:bg-gray-800 group-ui-open:bg-gray-800">
           <div className="flex-shrink-0">
-            {session ? (
+            {picture ? (
               <picture className="relative flex justify-center align-center">
-                <img
-                  src={session?.user?.image as string}
-                  alt=""
-                  className="w-8"
-                />
+                <img src={picture as string} alt="" className="w-8" />
               </picture>
             ) : (
               <div className="w-8 h-8 bg-primary animate-pulse" />
             )}
           </div>
           <div className="grow overflow-hidden text-ellipsis whitespace-nowrap text-left text-white">
-            {session ? (
-              <span>{session?.user?.email}</span>
+            {email ? (
+              <span>{email}</span>
             ) : (
               <div className="w-40 h-8 bg-primary animate-pulse"></div>
             )}
