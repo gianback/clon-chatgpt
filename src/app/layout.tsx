269838ce6f7e065d76b44f1218d92d66ba9d1ef0
Gian Franco <gianfraback@gmail.com>
2023-09-03 15:12:42 -0500

    refactor: add userStoreIntializer in mainLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,31 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { NextAuthProvider } from "@/components";
+import { Session, getServerSession } from "next-auth";
+import { authOptions } from "@/utilities/auth.utility";
+import { UserStoreInitializer } from "@/components/UserStoreInitializer";
+import { userStore } from "@/store/userStore";
 
 export const metadata: Metadata = {
   title: "Clone ChatGpt",
   description: "A funny ChatGPT clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = (await getServerSession(authOptions)) as Session;
+  userStore.setState({
+    email: session?.user?.email,
+    id: session?.user?.id,
+    picture: session?.user?.image,
+  });
   return (
     <html lang="en">
       <body>
+        <UserStoreInitializer session={session} />
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
