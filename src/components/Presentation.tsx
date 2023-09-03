ea396c35c6a28b76cadd043e837b16982076ba3e
Gian Franco <gianfraback@gmail.com>
2023-09-03 15:14:31 -0500

    refactor: removed dark class

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -20,7 +20,7 @@ export function Presentation() {
           <div className="h-full flex flex-col ">
             {isLoading ? <Spinner /> : <CurrentHistory />}
             <div className="h-[80px] lg:h-[90px] w-full" />
-            <div className="bg-primary absolute bottom-[5px] left-0 w-full border-t border-tertiary md:border-transparent md:dark:border-transparent md:bg-vert-light-gradien dark:bg-gray-800 dark:md:bg-vert-dark-gradient pt-2 pb-2 md:pl-2">
+            <div className="bg-primary absolute bottom-[5px] left-0 w-full border-t border-tertiary md:border-transparent md md:bg-vert-light-gradien  pt-2 pb-2 md:pl-2">
               <Form />
             </div>
           </div>
