49ff1acf952a42d8fe4675fc7e0e3097badd3e98
Gian Franco <gianfraback@gmail.com>
2023-09-03 04:01:15 -0500

    feat: add userStoreInitializer

--- src/app/home/query/[id]/page.tsx
+++ src/app/home/query/[id]/page.tsx
@@ -1,16 +1,20 @@
-"use client";
-
 import { Overlay, Presentation } from "@/components";
-import { saveQueryListUtility } from "@/utilities/saveQueryList.utility";
-import { useEffect } from "react";
+import { CurrentHistoryInitializer } from "@/components/CurrentHistoryInitializer";
+import { getHistoryByIdService } from "@/services/getHistoryById.service";
 
-export default function QueryDetails({ params }: { params: { id: string } }) {
-  useEffect(() => {
-    saveQueryListUtility(params.id);
-  }, [params.id]);
+export default async function QueryDetails({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const response = await getHistoryByIdService(params.id);
 
   return (
     <main className="h-screen overflow-hidden w-full relative flex flex-col z-0 lg:flex-row">
+      <CurrentHistoryInitializer
+        currentHistory={response.querys}
+        paramsId={params.id}
+      />
       <Presentation />
       <Overlay />
     </main>
