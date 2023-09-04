cd1eeee0134a17960a3669ce7ba77c70f36d2a2b
Gian Franco <gianfraback@gmail.com>
2023-09-03 22:43:28 -0500

    refactor: refactor setUserStore to home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,24 @@
-import { getServerSession } from "next-auth";
+import { Session, getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { Overlay, Presentation } from "@/components";
 import { authOptions } from "@/utilities/auth.utility";
+import { Overlay, Presentation } from "@/components";
+import { UserStoreInitializer } from "@/components/UserStoreInitializer";
+import { userStore } from "@/store/userStore";
 
 export default async function Main() {
-  const session = await getServerSession(authOptions);
+  const session = (await getServerSession(authOptions)) as Session;
   if (!session) {
     redirect("/auth/signin");
   }
+  userStore.setState({
+    email: session?.user?.email,
+    id: session?.user?.id,
+    picture: session?.user?.image,
+  });
 
   return (
     <main className="h-screen overflow-hidden w-full relative flex flex-col z-0 lg:flex-row">
+      <UserStoreInitializer session={session} />
       <Presentation />
       <Overlay />
     </main>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+
 import { NextAuthProvider } from "@/components";
-import { Session, getServerSession } from "next-auth";
-import { authOptions } from "@/utilities/auth.utility";
-import { UserStoreInitializer } from "@/components/UserStoreInitializer";
-import { userStore } from "@/store/userStore";
 
 export const metadata: Metadata = {
   title: "Clone ChatGpt",
@@ -16,16 +13,9 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = (await getServerSession(authOptions)) as Session;
-  userStore.setState({
-    email: session?.user?.email,
-    id: session?.user?.id,
-    picture: session?.user?.image,
-  });
   return (
     <html lang="en">
       <body>
-        <UserStoreInitializer session={session} />
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
