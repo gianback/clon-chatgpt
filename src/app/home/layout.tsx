29fa55c765a7e04c3d28c39548ec1f62de02d40b
Gian Franco <gianfraback@gmail.com>
2023-09-03 23:25:35 -0500

    refactor: moved redirect to layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Header, HistorySection } from "@/components";
-
+import { UserStoreInitializer } from "@/components/UserStoreInitializer";
+import { userStore } from "@/store/userStore";
+import { Session, getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/utilities/auth.utility";
 import "../globals.css";
 
 export const metadata: Metadata = {
@@ -13,10 +17,20 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = (await getServerSession(authOptions)) as Session;
+  if (!session) {
+    redirect("/auth/signin");
+  }
+  userStore.setState({
+    email: session?.user?.email,
+    id: session?.user?.id,
+    picture: session?.user?.image,
+  });
   return (
     <html lang="en">
       <body>
         <div className="h-screen overflow-hidden w-full relative flex flex-col z-0 lg:flex-row">
+          <UserStoreInitializer session={session} />
           <Header>
             <HistorySection />
           </Header>
